c9c6a402842db97ff55055a2817b43b63db99899
superfroggman <49985901+superfroggman@users.noreply.github.com>
2020-08-28 19:00:19 +0200

    Added QnA

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,30 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Niklas!");
+
+            Console.WriteLine("Namn?");
+            String name = Console.ReadLine();
+
+
+            Console.WriteLine("Ålder?");
+            String age = Console.ReadLine();
+
+            Console.WriteLine("Levande?");
+            String alive = Console.ReadLine();
+
+            Console.WriteLine("\nNamn: " + name);
+            Console.WriteLine("Ålder: " + age);
+            Console.WriteLine("Levande: " + alive);
+
+            try
+            {
+                int age2 = int.Parse(age);
+                Console.WriteLine("Ålder i kvadrat: " + Math.Pow(age2, 2));
+            }
+            catch
+            {
+                Console.WriteLine("Dålig ålder!");
+            }
         }
     }
 }

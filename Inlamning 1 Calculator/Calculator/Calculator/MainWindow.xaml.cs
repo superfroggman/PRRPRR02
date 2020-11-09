ab259ca4e243b5b823a22ec403648e04bd6832ef
superfroggman <49985901+superfroggman@users.noreply.github.com>
2020-11-09 12:47:59 +0100

    started working on calculator

--- Inlamning 1 Calculator/Calculator/Calculator/MainWindow.xaml.cs
+++ Inlamning 1 Calculator/Calculator/Calculator/MainWindow.xaml.cs
@@ -20,9 +20,41 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        int gridWidth = 5;
+        int gridHeight = 5;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //Set grid to correct size
+            for (int i = 0; i < gridWidth; i++)
+            {
+                mainGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int i = 0; i < gridHeight; i++)
+            {
+                mainGrid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            string[,] buttons = new string[,] {
+                {"7", "8", "9"},
+                {"4", "5", "6"},
+                {"1", "2", "3"},
+                {"", "0", ""}
+            };
+
+            //Create buttons 1-9
+            for (int i = 1; i <= 9; i++)
+            {
+                var button = new Button();
+                button.SetValue(Grid.ColumnProperty, (i-1)%3);
+                button.SetValue(Grid.RowProperty, (i-1)/3);
+                button.SetValue(ContentProperty, i);
+                mainGrid.Children.Add(button);
+            }
+
         }
     }
 }

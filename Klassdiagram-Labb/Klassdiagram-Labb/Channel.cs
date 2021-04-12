d863cb2c97bd7cf4748b2aaa13b27ed0cc0d4828
superfroggman <49985901+superfroggman@users.noreply.github.com>
2021-04-12 12:08:52 +0200

    missed this file

---
+++ Klassdiagram-Labb/Klassdiagram-Labb/Channel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Klassdiagram_Labb
+{
+    class Channel
+    {
+        private string name;
+
+        public List<string> contents;
+
+        public Channel(string name)
+        {
+
+        }
+
+        public string GetName()
+        {
+
+        }
+
+        public void Subscribe(IUser user)
+        {
+
+        }
+
+        public void Unsubscribe(IUser user)
+        {
+
+        }
+
+        public void AddContent(string content)
+        {
+
+        }
+    }
+}

e44a299f9593e0da95e4233d9a29aa125db99020
superfroggman <49985901+superfroggman@users.noreply.github.com>
2021-04-27 12:12:36 +0200

    started implementing code generated buttons

---
+++ Slutprojekt/Slutprojekt/Buttons/GotchiButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slutprojekt
+{
+    class GotchiButton : IButton
+    {
+        public string iconLocation { get; }
+
+        public GotchiButton(string location)
+        {
+            iconLocation = location;
+        }
+    }
+}
---
+++ Slutprojekt/Slutprojekt/Buttons/IStatusProperties.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slutprojekt
+{
+    interface IStatusProperties : ITiredness
+    {
+    }
+}
